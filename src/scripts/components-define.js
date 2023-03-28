2ccec1ab7372376d64dcd9f6e64a64c2b9ca02c2
KeidsID <keidscode@gmail.com>
2023-03-28 19:23:04 +0800

    Simple <search-bar> DONE

--- src/scripts/components-define.js
+++ src/scripts/components-define.js
@@ -1,3 +1,5 @@
 import AppBar from './components/app-bar.js';
+import SearchBar from './components/search-bar.js';
 
 customElements.define(AppBar.name, AppBar);
+customElements.define(SearchBar.name, SearchBar);
